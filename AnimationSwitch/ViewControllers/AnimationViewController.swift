005d8499df8619cfc4aa8d0320a5d7cb9daad747
Pavel Gribachev <PGribachev@yandex.ru>
2024-01-26 14:54:49 +0300

    first step

--- AnimationSwitch/ViewControllers/AnimationViewController.swift
+++ AnimationSwitch/ViewControllers/AnimationViewController.swift
@@ -19,14 +19,24 @@ final class AnimationViewController: UIViewController {
     @IBOutlet weak var animationSpringView: SpringView!
     @IBOutlet weak var changingAnimationButton: SpringButton!
     
+    private let animation = MainAnimation().preset
+    private var animationIndex = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+//        animation.forEach { print($0) }
     }
     
     @IBAction func changingSpringViewDidTapped(_ sender: SpringButton) {
         
+        changeAnimation()
+        animationSpringView.animate()
+        animationIndex += 1
     }
-    
 }
 
+private extension AnimationViewController {
+    func changeAnimation() {
+        animationSpringView.animation = String(animation[animationIndex])
+    }
+}
