6ab10fe48ebed68b723acaef7200f89af0b455b2
Pavel Gribachev <PGribachev@yandex.ru>
2024-01-26 18:17:27 +0300

    STEP 1 - The rough work is done, the application is working.

--- AnimationSwitch/Models/Model.swift
+++ AnimationSwitch/Models/Model.swift
@@ -9,45 +9,6 @@ import Foundation
 import SpringAnimation
 
 struct MainAnimation {
-    
-    let preset = AnimationPreset.allCases.map { $0.rawValue }
-    
-//    let preset = [Preset]()
-//    
-//    func getPreset() -> [String] {
-//        
-//        let stringPreset = preset.map { $0.rawValue }
-//        return stringPreset
-//        
-//    }
-}
-
-enum Preset: String {
-    case pop = "pop"
-    case slideLeft = "slideLeft"
-    case slideRight = "slideRight"
-    case slideDown = "slideDown"
-    case slideUp = "slideUp"
-    case squeezeLeft = "squeezeLeft"
-    case squeezeRight = "squeezeRight"
-    case squeezeDown = "squeezeDown"
-    case squeezeUp = "squeezeUp"
-    case fadeIn = "fadeIn"
-    case fadeOut = "fadeOut"
-    case fadeOutIn = "fadeOutIn"
-    case fadeInLeft = "fadeInLeft"
-    case fadeInRight = "fadeInRight"
-    case fadeInDown = "fadeInDown"
-    case fadeInUp = "fadeInUp"
-    case zoomIn = "zoomIn"
-    case zoomOut = "zoomOut"
-    case fall = "fall"
-    case shake = "shake"
-    case flipX = "flipX"
-    case flipY = "flipY"
-    case morph = "morph"
-    case squeeze = "squeeze"
-    case flash = "flash"
-    case wobble = "wobble"
-    case swing = "swing"
+    let preset = AnimationPreset.allCases.map { $0.rawValue }.shuffled()
+    let curve = AnimationCurve.allCases.map { $0.rawValue }.shuffled()
 }
--- AnimationSwitch/ViewControllers/AnimationViewController.swift
+++ AnimationSwitch/ViewControllers/AnimationViewController.swift
@@ -16,15 +16,45 @@ final class AnimationViewController: UIViewController {
     @IBOutlet weak var durationLabel: UILabel!
     @IBOutlet weak var delayLabel: UILabel!
     
-    @IBOutlet weak var animationSpringView: SpringView!
-    @IBOutlet weak var changingAnimationButton: SpringButton!
+    @IBOutlet weak var animationSpringView: SpringView! {
+        didSet {
+            animationSpringView.layer.cornerRadius = 10
+        }
+    }
+    @IBOutlet weak var animationChangeButton: SpringButton!{
+        didSet {
+            animationChangeButton.layer.cornerRadius = 10
+        }
+    }
     
-    private let animation = MainAnimation().preset
-    private var animationIndex = 0
+    private let preset = MainAnimation().preset
+    private let curve = MainAnimation().curve
+
+    private var animationIndex = 0 {
+        didSet {
+            if animationIndex == preset.count {
+                animationIndex = 0
+                
+                animationChangeButton.setTitle("Run again", for: .normal)
+            }
+        }
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
-//        animation.forEach { print($0) }
+        
+        animationSpringView.animation = preset[animationIndex]
+        animationSpringView.curve = curve[animationIndex]
+        animationSpringView.force = 1.00
+        animationSpringView.duration = 0.50
+        animationSpringView.delay = 1.33
+        
+        presetLabel.text = "preset: \(preset[animationIndex])"
+        curveLabel.text = "curve: \(curve[animationIndex])"
+        forceLabel.text = "force: 1.00"
+        durationLabel.text = "duration: 0.50"
+        delayLabel.text = "delay: 1.33"
+        
     }
     
     @IBAction func changingSpringViewDidTapped(_ sender: SpringButton) {
@@ -37,6 +67,32 @@ final class AnimationViewController: UIViewController {
 
 private extension AnimationViewController {
     func changeAnimation() {
-        animationSpringView.animation = String(animation[animationIndex])
+        
+        let force = randomDoubleNumber()
+        let duration = randomDoubleNumber()
+        let delay = randomDoubleNumber()
+        
+        animationSpringView.animation = preset[animationIndex]
+        animationSpringView.curve = curve[animationIndex]
+        animationSpringView.force = force
+        animationSpringView.duration = duration
+        animationSpringView.delay = delay
+        
+        presetLabel.text = "preset: \(preset[animationIndex])"
+        curveLabel.text = "curve: \(curve[animationIndex])"
+        forceLabel.text = "force: \(force)"
+        durationLabel.text = "duration: \(duration)"
+        delayLabel.text = "delay: \(delay)"
+        
+        animationChangeButton.setTitle("Run \(preset[animationIndex + 1])", for: .normal)
+    }
+    
+    func randomDoubleNumber() -> Double {
+        let number = Double.random(in: 0.3...1.5)
+        return roundDecimal(places: number)
+    }
+    
+    func roundDecimal(places double: Double) -> Double {
+        return Double(round(100 * double) / 100)
     }
 }
